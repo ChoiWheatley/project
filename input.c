e36c4c768f2e54e39a442844bbc513f66b5808dc
ChoiWheatley <chltmdgus604@naver.com>
2016-06-05 16:11:08 +0100

    input.c 파일을 수정하고 시그윈에서 커밋함.

--- input.c
+++ input.c
@@ -1,21 +1,47 @@
 #include <stdio.h>
 int main(void)
 {
+	int k = 0;
 	int c;
-	int i = 0;		//반복용
-	int count;		//몇 번째 입력받은 숫자인지
+	char tmp[61] = {0};
+	int tmpcnt = 0;
 	char num[1000][61];
-	
+	int count = 1;
+	int countk = 0;
 	printf("hello. input number haha : ");
-	while ((c = getchar()) != '\n')
+	
+	for (int i = 0; (c = getchar()) != '\n'; count++, i++)
 	{
+		tmp[i] = c;
+		if (c == '.')
+			countk = count - 1;		// '.' 전까지의 개수를 잰다.
+	}
+
+	if (countk == 0)
+		countk = count - 1;
+	else							// '.'이 있다면 num[k][50]부터 소수자리수를 차례로 쓴다.
+		for (int i = 0; tmp[countk + 1 + i] != 0; ++i)
+			num[k][50 + i] = tmp[countk + 1 + i];
+
+	char stack[countk];
+
+	for (int i = 0; i < countk; i++)
+		stack[i] = tmp[i];
+
+	for (int i = 0; countk > 0; countk--, i++)
+		num[k][50 - countk] = stack[i];
+
+	for (int i = 0; i < 61; i++)
+		printf("num[%d][%d] = %c\n", k, i, num[k][i]);
+
+	/*		
 		switch (c) 
 		{
 			case ' ':
-				printf("hello\n");
+				printf("what 띄어쓰기\n");
 				break;
 			case '.':
-				printf("what the fuck?\n");
+				printf("test : count = %d\n", count);
 				break;
 			case '+':
 				printf("this is plus.\n");
@@ -42,14 +68,15 @@ int main(void)
 			case '8':
 			case '9':
 			case '0':
-				printf("this is fucking number %c\n", c);
+				printf("ok\n");
+				tmp[tmpcnt++] = c;
 				break;
 			default:
 				printf(" error\n");
 				break;
 		}
-		i++;
 	}
+	*/
 
 	return 0;
 }
