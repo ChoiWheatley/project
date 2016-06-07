8ed4b9da422a6ff371e4bb73b812829113f0f832
ChoiWheatley <chltmdgus604@naver.com>
2016-06-07 14:17:06 +0100

    hello

--- input.c
+++ input.c
@@ -8,22 +8,32 @@ int main(void)
 	char num[1000][61];
 	int count = 1;
 	int countk = 0;
+	int is_point = 0;
 	printf("hello. input number haha : ");
 	
 	for (int i = 0; (c = getchar()) != '\n'; count++, i++)
 	{
-		tmp[i] = c;
-		if (c == '.')
+		if ((c == '0') || (c == '1') || (c == '2') || (c == '3') || (c == '4') || (c == '5') || (c == '6') || (c == '7') || (c == '8') || (c == '9'));
+			tmp[i] = c;
+		else if (c == '.')
+		{
 			countk = count - 1;		// '.' 전까지의 개수를 잰다.
+			is_point = 1;
+		}
+		else if (c == ' ')
+		{
+			countk = count = 0;
+			is_point = 0;
+		}
 	}
 
 	if (countk == 0)
-		countk = count - 1;
+		countk = count - 1;			// '.'이 없다면 count 그대로 쓰는거지 ㅋ
 	else							// '.'이 있다면 num[k][50]부터 소수자리수를 차례로 쓴다.
 		for (int i = 0; tmp[countk + 1 + i] != 0; ++i)
 			num[k][50 + i] = tmp[countk + 1 + i];
 
-	char stack[countk];
+	char stack[countk];				//stack 배열에 맞추어 정수부분을 써내려간다. 
 
 	for (int i = 0; i < countk; i++)
 		stack[i] = tmp[i];
